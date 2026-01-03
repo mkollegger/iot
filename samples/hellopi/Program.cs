8f9e807302cc2a92f5882836782152d6e4aca430
Michael Kollegger <mike@fotec.at>
2026-01-03 19:53:09 +0100

    wip

---
+++ samples/hellopi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace hellopi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write ("Warte auf Debugger ...");
+
+            // Warten bis Debugger angehängt ist
+            int count = 0;
+            while (!Debugger.IsAttached)
+            {
+                Thread.Sleep(100);
+                count++;
+                if (count % 10 == 0)
+                {
+                    Console.Write(".");
+                }
+
+                if (count == 600)
+                {
+                    Console.WriteLine("\nZeitüberschreitung beim Warten auf Debugger.");
+                    break;
+                }
+            }
+            Console.WriteLine();
+
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Debugger angehängt. Fortfahren...");
+                Debugger.Break();
+            }
+
+            // Hier können Sie Ihren Code hinzufügen, der debuggt werden soll
+
+            Console.WriteLine("Programm beendet. Drücken Sie eine Taste zum Beenden.");
+        }
+    }
+}
